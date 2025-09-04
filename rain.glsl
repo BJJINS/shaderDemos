105d7988f2f8d969bc2b89bb82060191989c5f4c
jinbaojia <bjjins@163.com>
2025-09-04 16:50:08 +0800

    shader

---
+++ rain.glsl
@@ -0,0 +1,16 @@
+/* Main function, uniforms & utils */
+#ifdef GL_ES
+    precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform sampler2D u_texture_0;
+
+
+void main() {
+    vec2 st = gl_FragCoord.xy / u_resolution.xy;
+    vec3 color = texture2D(u_texture_0, st).rgb;
+    gl_FragColor = vec4(color, 1.0);
+}
