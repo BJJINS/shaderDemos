25d75ccc3992f41837cfae398b6879564bb8645c
jinbaojia <bjjins@163.com>
2025-09-16 00:10:23 +0800

    webgl temp

---
+++ pages/webgl/index.ts
@@ -0,0 +1,27 @@
+import { createProgram, createShader } from "./../js/webgl_utils";
+
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const gl = canvas.getContext("webgl2")!;
+gl.clearColor(108 / 255, 225 / 255, 153 / 255, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+const vertexShaderSource = `
+  #version 300 es
+  in vec2 a_position;
+  void main() {
+    gl_Position = vec4(a_position, 0, 1);
+  }
+`;
+const fragmentShaderSource = `
+  #version 300 es
+  precision mediump float;
+  out vec4 outColor;
+  void main() {
+    outColor = vec4(0.4745, 0.3333, 0.2823, 1);
+  }
+`;
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)!;
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)!;
+const program = createProgram(gl, vertexShader, fragmentShader);
