1c40286a8558eacc9cbe0658758ea3b5fb8828db
jinbaojia <bjjins@163.com>
2025-09-19 00:38:08 +0800

    整理代码

--- glsl/raytracing/index.glsl
+++ glsl/raytracing/index.glsl
@@ -8,59 +8,30 @@ out vec4 fragColor; // 输出颜色
 #include "constants.glsl" // 常量定义
 #include "sphere.glsl" // 球体相关定义和函数
 #include "box.glsl" // 立方体相关定义和函数
-#include "ray.glsl" // 射线相关定义和函数
 #include "plane.glsl" // 平面相关定义和函数
 #include "light.glsl" // 光照相关函数
+#include "ray.glsl" // 射线相关定义和函数
 
-// 修复的光线追踪函数
+// vec4 rayReflect(in vec3 rayOrigin, in vec3 rayDirection, float min_t, float max_t) {
+
 vec4 raytracing(in vec3 rayOrigin, in vec3 rayDirection, float min_t, float max_t) {
-    float closestT = max_t;
     vec3 finalColor = backgroundColor;
-    vec3 hitNormal;
-    bool hitSomething = false;
-    vec3 objectColor = vec3(0.0);
-    float specularPower = 1.0; // 高光反射指数
-    bool isPlane = false; // 地面步使用光照
 
-    // 计算最近的球体相交
-    Sphere hitSphere;
-    vec4 hitSphereNormalT = closestSpheresIntersection(rayOrigin, rayDirection, min_t, max_t, hitSphere);
-    if (hitSphereNormalT.w > 0.0 && hitSphereNormalT.w < closestT) {
-        hitNormal = hitSphereNormalT.xyz; // 球体法线
-        closestT = hitSphereNormalT.w; // 球体相交解t
-        hitSomething = true;
-        objectColor = hitSphere.color;
-        specularPower = hitSphere.specularPower;
-    }
+    mat3x3 hitInfo = raytracingImpl(rayOrigin, rayDirection, min_t, max_t);
+    vec3 hitNormal = hitInfo[0];
+    vec3 objectColor = hitInfo[1];
+    float specularPower = hitInfo[2].x;
+    float closestT = hitInfo[2].y;
+    float hitType = hitInfo[2].z;
 
-    // 计算最近的立方体相交
-    Box hitBox;
-    vec4 hitBoxNormalT = closestBoxIntersection(rayOrigin, rayDirection, min_t, max_t, hitBox);
-    if (hitBoxNormalT.w > 0.0 && hitBoxNormalT.w < closestT) {
-        hitNormal = hitBoxNormalT.xyz; // 立方体法线
-        closestT = hitBoxNormalT.w; // 立方体相交解t
-        hitSomething = true;
-        objectColor = hitBox.color;
-        specularPower = hitBox.specularPower;
-    }
-
-    // 计算最近的地面相交
-    vec4 hitPlaneNormalT = closestPlaneIntersection(rayOrigin, rayDirection, groundPlane, min_t, max_t);
-    if (hitPlaneNormalT.w > 0.0 && hitPlaneNormalT.w < closestT) {
-        hitNormal = hitPlaneNormalT.xyz; // 地面法线
-        closestT = hitPlaneNormalT.w; // 地面相交解t
-        hitSomething = true;
-        objectColor = groundColor;
-        isPlane = true;
-    }
-    if (hitSomething) {
+    if (hitType >= 0.0) {
         vec3 hitPoint = rayOrigin + rayDirection * closestT;
         vec3 viewDirection = normalize(rayOrigin - hitPoint);
 
-        // 阴影优化：修复阴影检测逻辑和改进阴影颜色
+        // 阴影检测
         float shadowFactor = shadow(hitPoint, lightPos);
-        
-        if (isPlane) {
+
+        if (hitType == 2.0) { // 地板
             finalColor = objectColor * shadowFactor; // 对平面应用阴影
         } else {
             // 环境光（降低强度以便看到其他光照效果）
--- glsl/raytracing/ray.glsl
+++ glsl/raytracing/ray.glsl
@@ -20,3 +20,51 @@ vec3 ray(vec3 rayOrigin, vec3 target, float d) {
         );
     return viewPortMatrix * direction;
 }
+
+mat3x3 raytracingImpl(in vec3 rayOrigin, in vec3 rayDirection, float min_t, float max_t) {
+    float closestT = max_t;
+    vec3 hitNormal;
+    float specularPower = 1.0; // 高光反射指数
+    vec3 hitColor = vec3(0.0);
+    float hitType = -1.0; // 0: 球体, 1: 立方体, 2: 地面, -1 无碰撞
+
+    // 计算最近的球体相交
+    Sphere hitSphere;
+    vec4 hitSphereNormalT = closestSpheresIntersection(rayOrigin, rayDirection, min_t, max_t, hitSphere);
+    if (hitSphereNormalT.w > 0.0 && hitSphereNormalT.w < closestT) {
+        hitNormal = hitSphereNormalT.xyz; // 球体法线
+        closestT = hitSphereNormalT.w; // 球体相交解t
+        hitColor = hitSphere.color;
+        specularPower = hitSphere.specularPower;
+        hitType = 0.0;
+    }
+
+    // 计算最近的立方体相交
+    Box hitBox;
+    vec4 hitBoxNormalT = closestBoxIntersection(rayOrigin, rayDirection, min_t, max_t, hitBox);
+    if (hitBoxNormalT.w > 0.0 && hitBoxNormalT.w < closestT) {
+        hitNormal = hitBoxNormalT.xyz; // 立方体法线
+        closestT = hitBoxNormalT.w; // 立方体相交解t
+        hitColor = hitBox.color;
+        specularPower = hitBox.specularPower;
+        hitType = 1.0;
+    }
+
+    // 计算最近的地面相交
+    vec4 hitPlaneNormalT = closestPlaneIntersection(rayOrigin, rayDirection, groundPlane, min_t, max_t);
+    if (hitPlaneNormalT.w > 0.0 && hitPlaneNormalT.w < closestT) {
+        hitNormal = hitPlaneNormalT.xyz; // 地面法线
+        closestT = hitPlaneNormalT.w; // 地面相交解t
+        hitColor = groundColor;
+        hitType = 2.0;
+    }
+
+    return mat3x3(
+        hitNormal, // 交点法线
+        hitColor, // 交点颜色
+        vec3(
+            specularPower, // 交点高光反射指数
+            closestT, // 交点最近距离解
+            hitType // 交点类型
+        ));
+}
