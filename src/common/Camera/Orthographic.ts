6f4750a092f3711ec358e43697ff836b60d9898d
jinbaojia <bjjins@163.com>
2025-10-21 15:15:48 +0800

    正交投影矩阵修改

--- src/common/Camera/Orthographic.ts
+++ src/common/Camera/Orthographic.ts
@@ -4,28 +4,25 @@ import Camera from "./Camera";
 
 class OrthographicCamera extends Camera {
   projectionMatrix: Float32Array;
-  constructor(left:number, right:number, bottom:number, top:number, near:number, far:number) {
+  constructor(l:number, r:number, b:number, t:number, n:number, f:number) {
     super("orthographic");
 
-    near = -near;
-    far = -far;
-    const w = right - left;
-    const h = top - bottom;
-    const d = far - near;
+    n = -n;
+    f = -f;
 
     // 将视锥体范围缩放到 -2到2（Z 轴需为负以匹配 OpenGL 的右手坐标与深度方向）
     const scaleMat4 = new Mat4([
-      2/w, 0,   0,   0,
-      0,   2/h, 0,   0,
-      0,   0,  2/d, 0,
-      0,   0,   0,   1
+      2/(r-l), 0,       0,       0,
+      0,       2/(t-b), 0,       0,
+      0,       0,       2/(n-f), 0,
+      0,       0,       0,       1
     ]);
 
     // 将视锥体的中心平移到坐标原点
     const translateMat4 = new Mat4([
-      1, 0, 0, -(left + right) / 2,
-      0, 1, 0, -(top + bottom) / 2,
-      0, 0, 1,  -(far + near)   / 2,
+      1, 0, 0, -(l + r) / 2,
+      0, 1, 0, -(t + b) / 2,
+      0, 0, 1, -(f + n) / 2,
       0, 0, 0, 1
     ]);
 
